f36c85125bcffdaebe628cddc2ed254e171a816e
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-18 02:35:29 +0600

    how to make component with function

--- src/App.js
+++ src/App.js
@@ -19,31 +19,35 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude. React. How are you?</h3>
-        </div>
 
-        <div className="music">
-          <p>Name: {2222 + number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p style={{ color: 'blue', backgroundColor: 'yellow' }}>Name: {singer2.name} {singer2.job}</p>
-        </div>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>new component</h5>
+      <Friend></Friend>
+      <Friend></Friend>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div className='person'>
+      <h1>Sakib al Hasan</h1>
+      <p>Profession: criceketer</p>
+    </div>
+  );
+}
+
+function Friend() {
+
+  return (
+    <div className='container'>
+      <h3>Name: Ajay devgun</h3>
+      <p>job: maramari</p>
     </div>
   );
 }
